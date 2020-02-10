e37730719055c474b79ab48290c75050be10de56
ehgus8656 <ehak159@gmail.com>
2020-02-10 17:41:40 +0900

    Spring03

--- src/main/java/assembler/Assembler.java
+++ src/main/java/assembler/Assembler.java
@@ -4,6 +4,11 @@ import spring01.ChangePasswordService;
 import spring01.MemberDao;
 import spring01.MemberRegisterService;
 
+/*
+ * 조립기는 객체를 생성하고 의존 객체를 주입해주는 기능을 제공
+ * 또한 특정 객체를 필요로 하는 곳에 객체를 제공할 수 있다.
+ */
+
 public class Assembler {
 	private MemberDao memberDao;
 	private MemberRegisterService regSvc;
